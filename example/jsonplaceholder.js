67ab78b56c3a204d2e0518af63a2694708313412
avne <andre.v.neumann@gmx.de>
2015-08-09 11:56:34 +0200

    Added more output; Fixed require statement.

--- example/jsonplaceholder.js
+++ example/jsonplaceholder.js
@@ -1,20 +1,25 @@
 var http = require('http'),
-    Promise = require("ES5Promise");
+    Promise = require("../src/Promise.js");
 
 new Promise(function (resolve, reject) {
+    console.log("Calling jsonplaceholder REST API");
+
     http.get("http://jsonplaceholder.typicode.com/posts/1", function(response) {
         var data = "";
 
         response.setEncoding("utf-8");
 
         response.on("data", function(chunk) {
+            console.log("data chunk received: " + chunk);
             data += chunk;
         });
 
         response.on("end", function() {
+            console.log("transfer finished");
             resolve(data);
         });
     }).on("error", function(error) {
+        console.error("An error occurred: " + error.message);
         reject(error);
     });
 }).then(function (data) {
